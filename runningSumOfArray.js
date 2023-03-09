2cdaaad365654423516eb058bd07c82c54793de5
KaPlaneta <planeta.karolinaa@gmail.com>
2023-03-09 12:45:12 +0100

    feat: add exercise

---
+++ runningSumOfArray.js
@@ -0,0 +1,13 @@
+// Given an array nums. We define a running sum of an array as runningSum[i] = sum(nums[0]…nums[i]).
+
+// Return the running sum of nums.
+
+var runningSum = function (nums) {
+  let sum = 0;
+  const result = [];
+  for (let i = 0; i < nums.length; i++) {
+    sum += nums[i];
+    result.push(sum);
+  }
+  return result;
+};
